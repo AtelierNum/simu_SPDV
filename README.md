2cd75b8564657975fd4c5faaf8175e385ecb6a46
valentin dupas <valentin.lc.dupas@gmail.com>
2023-10-19 14:11:09 +0200

    MVP

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # simu_SPDV
+
+This will simulate OSC output of the LiDar system installed by 61fps.
+
+It will randomly fill the blob arrays with a 30/70 distribution of live/dead blobs. Each blobs move by walking inside a simplex noise.
+
+# Install the dependencies
+
+```
+npm install
+```
+
+# Run the project
+
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,163 @@
+// TODO : randomly drop a blop and/or create a new one
+
+import { Bundle, Client, Server } from "node-osc";
+import { createNoise2D } from "simplex-noise";
+
+const PORT = 10000;
+
+const FLOOR_WALL_TX_MAXIMUM = 5.863999843597412;
+const FLOOR_WALL_TY_MAXIMUM = 6.25;
+const FLOOR_TX_MAXIMUM = 5.863999843597412;
+const FLOOR_TY_MAXIMUM = 3.125;
+const WALL_TX_MAXIMUM = 5.863999843597412;
+const WALL_TY_MAXIMUM = 3.125;
+
+function* idGenerator(id) {
+  while (true) {
+    id += 1;
+    yield id;
+  }
+}
+
+const idIterator = idGenerator(Math.floor(Math.random() * 10000) + 1);
+
+let wall = getBlobArray(10);
+let floor = getBlobArray(20);
+const noise2D = createNoise2D();
+const NOISE_WALKING_SPEED = 0.00001;
+
+// // lOOPBACK LISTENER FOR TESTING
+// let log = "";
+
+// const oscServer = new Server(PORT, "0.0.0.0", () => {
+//   console.log("Server started");
+// });
+
+// oscServer.on("message", function (msg) {
+//   console.log(`Message: ${msg}`);
+//   log += "\n" + msg;
+// });
+
+// oscServer.on("bundle", function (bundle) {
+//   bundle.elements.forEach((element, i) => {
+//     if (bundle.timetag[i]) {
+//       console.log(`Timestamp: ${bundle.timetag[i]}`);
+//     }
+
+//     console.log("" + element);
+//   });
+// });
+
+const client = new Client("127.0.0.1", PORT);
+
+function getBlobArray(size) {
+  let blobs = new Array(size);
+
+  for (let i = 0; i < size; i++) {
+    if (Math.random() < 0.7) {
+      blobs[i] = {
+        id: 0,
+        // tx: 0, // redundant, will be derived from u
+        // ty: 0, // redundant, will be derived from v
+        u: 0,
+        v: 0,
+      };
+    } else {
+      blobs[i] = {
+        id: idIterator.next().value,
+        // tx: 0, // redundant, will be derived from u
+        // ty: 0, // redundant, will be derived from v
+        u: 0,
+        v: 0,
+      };
+    }
+  }
+
+  return blobs;
+}
+
+function updateBlobArray(blobs) {
+  for (let i = 0; i < blobs.length; i++) {
+    if (blobs[i].id == 0) {
+      continue;
+    }
+
+    blobs[i].u =
+      noise2D(Date.now() * NOISE_WALKING_SPEED + blobs[i].id * 10000, 0) * 0.5 +
+      0.5;
+    blobs[i].v =
+      noise2D(0, Date.now() * NOISE_WALKING_SPEED + blobs[i].id * 10000) * 0.5 +
+      0.5;
+  }
+}
+
+while (true) {
+  updateBlobArray(floor);
+  updateBlobArray(wall);
+
+  let bundleContent = [["/_samplerate", 60]];
+
+  const floor_wallPart = wall
+    .concat(floor)
+    .map((blob, i) => {
+      const prefix = `/SOL_MUR/blobs/blob${i}/`;
+
+      return [
+        [prefix + "id", blob.id],
+        [prefix + "tx", blob.u * FLOOR_WALL_TX_MAXIMUM],
+        [prefix + "ty", blob.v * FLOOR_WALL_TY_MAXIMUM],
+        [prefix + "u", blob.u],
+        [prefix + "v", blob.v],
+      ];
+    })
+    .flat(1);
+
+  const floor_wallSpecs = [
+    ["/SOL_MUR/specs/Scalex", FLOOR_WALL_TX_MAXIMUM],
+    ["/SOL_MUR/specs/Scaley", FLOOR_WALL_TY_MAXIMUM],
+  ];
+
+  const wallPart = wall.map((blob, i) => {
+    const prefix = `/MUR/blobs/blob${i}/`;
+
+    return [
+      [prefix + "id", blob.id],
+      [prefix + "tx", blob.u * WALL_TX_MAXIMUM],
+      [prefix + "ty", blob.v * WALL_TY_MAXIMUM],
+      [prefix + "u", blob.u],
+      [prefix + "v", blob.v],
+    ];
+  });
+
+  const wallSpecs = [
+    ["/MUR/specs/Scalex", WALL_TX_MAXIMUM],
+    ["/MUR/specs/Scaley", WALL_TY_MAXIMUM],
+  ];
+
+  const floorPart = floor.map((blob, i) => {
+    const prefix = `/SOL/blobs/blob${i}/`;
+
+    return [
+      [prefix + "id", blob.id],
+      [prefix + "tx", blob.u * FLOOR_TX_MAXIMUM],
+      [prefix + "ty", blob.v * FLOOR_TY_MAXIMUM],
+      [prefix + "u", blob.u],
+      [prefix + "v", blob.v],
+    ];
+  });
+
+  const floorSpecs = [
+    ["/SOL/specs/Scalex", FLOOR_TX_MAXIMUM],
+    ["/SOL/specs/Scaley", FLOOR_TY_MAXIMUM],
+  ];
+
+  const finalBundle = bundleContent
+    .concat(floor_wallPart)
+    .concat(floor_wallSpecs)
+    .concat(wallPart)
+    .concat(wallSpecs)
+    .concat(floorPart)
+    .concat(floorSpecs);
+
+  client.send(new Bundle(...finalBundle));
+}
